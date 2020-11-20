ca205abecf938f669a77273e26fa4d6e533884c8
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-19 20:46:49 -0800

    commit tree 3ff2d5513d9638bfc1434537eab3d90d0f58416c

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,42 @@
 class Instructor
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
 
+    def passed_students
+        self.instructed_students.select{|inst| inst.status == "passed"}
+    end
+    def pass_student(student, test)
+        if self.find_student_test(student, name)
+            self.find_student_test(student, name).status == "passed"
+        else 
+            BoatingTest.new(student, test, "passed", self)
+        end
+    end
+
+    def fail_student(student, name)
+        if self.find_student_test(student, name)
+            self.find_student_test(student, name).status == "failed"
+        else 
+            BoatingTest.new(student, test, "failed", self)
+        end
+    end
+
+    def find_student_test(student, name)
+        BoatingTest.all.find{|inst| inst.student == student && inst.name == name}
+    end
+
+    def all_students
+        self.instructed_students.map{|inst| inst.student.first_name}
+    end
+
+    def instructed_students
+        students = BoatingTest.all.select{|inst| inst.instructor == self}
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,33 @@
 class Student
+    attr_accessor :first_name
+    @@all = []
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(name, status, instructor)
+        BoatingTest.new(self, name, status, instructor)
+    end
+
+    def all_instructors
+        self.test_student.map {|test| test.instructor}
+    end
+
+    def find_student(first_name)
+        self.all.find{|inst| inst.first_name == self} 
+    end
+
+    def grade_percentage
+        pass = self.test_student.select{|inst| inst.status == "passed"}
+        pass.count.to_f / self.test_student.count.to_f
+    end
+
+    def test_student
+        BoatingTest.all.select{|test| test.student == self}
+    end
 end
