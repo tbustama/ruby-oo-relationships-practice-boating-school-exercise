68cc84f75878d53f38afb020993131e6e90b9606
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-20 06:04:16 -0800

    Completed boating lab

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,45 @@
 class Instructor
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
 
+    def passed_students
+        self.instructed_students.select{|inst| inst.status = "passed"}
+    end
+
+    def pass_student(student, name)
+        if self.find_student_test(student, name)
+            self.find_student_test(student, name).status = "passed"
+            self.find_student_test(student, name)
+        else 
+            BoatingTest.new(student, name, "passed", self)
+        end
+    end
+
+    def fail_student(student, name)
+        if self.find_student_test(student, name)
+            self.find_student_test(student, name).status = "failed"
+            self.find_student_test(student, name)
+        else 
+            BoatingTest.new(student, name, "failed", self)
+        end
+    end
+
+    def find_student_test(student, test_name)
+        BoatingTest.all.find{|inst| inst.student == student && inst.name == test_name}
+    end
+
+    def all_students
+        self.instructed_students.map{|inst| inst.student.first_name}
+    end
+
+    def instructed_students
+        BoatingTest.all.select{|inst| inst.instructor == self}
+    end
 end
